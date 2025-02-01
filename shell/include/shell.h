45fdf975f39d99a355f8e682f6df0401e7fc1ef5
AayushBade14 <aayushrajubade@gmail.com>
2025-02-01 16:52:38 +0530

    Beautifying/Cleaning files

--- shell/include/shell.h
+++ shell/include/shell.h
@@ -1,11 +1,23 @@
+/* 
+  ___   _     ______       _____ _   _  _____ _      _     
+ / _ \ | |    | ___ \     /  ___| | | ||  ___| |    | |    
+/ /_\ \| |    | |_/ /_____\ `--.| |_| || |__ | |    | |    
+|  _  || |    |  __/______|`--. \  _  ||  __|| |    | |    
+| | | || |____| |         /\__/ / | | || |___| |____| |____
+\_| |_/\_____/\_|         \____/\_| |_/\____/\_____/\_____/                      
+*/
+
 #pragma once
 
 #include "parser.h"
 #include "util.h"
 
 namespace AlpShell{
-
-  void run(char *flags);
+  
+  
+  void run(char **flags);
+  void run();
+  void alp_loop(char **flags);
   void alp_loop();
   void alp_prompt();
 
--- shell/src/shell.cpp
+++ shell/src/shell.cpp
@@ -1,13 +1,27 @@
 #include "shell.h"
 
-void AlpShell::run(char *flags){
-
-}
-
+// normal loop without arguments
 void AlpShell::alp_loop(){
 
 }
 
+// loop with arguments
+void AlpShell::alp_loop(char **flags){
+
+}
+
+// method for prompt
 void AlpShell::alp_prompt(){
 
 }
+
+// method to run shell with flags
+void AlpShell::run(char **flags){
+  AlpShell::alp_loop(flags);
+}
+
+// method to run shell without flags
+void AlpShell::run(){
+  AlpShell::alp_loop();
+}
+
