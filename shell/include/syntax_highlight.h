cfbfefb40b31b12e985d516df0ea130e0fee387c
Laksh-Mendpara <b23cs1037@iitj.ac.in>
2025-04-18 05:22:14 +0530

    Added: Syntax highlighting functions

---
+++ shell/include/syntax_highlight.h
@@ -0,0 +1,19 @@
+#ifndef SYNTAX_HIGHTLIGHT_H
+#define SYNTAX_HIGHTLIGHT_H
+
+// Add these near the top of main.cpp
+#include <iostream>
+#include <vector>
+#include <string>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include "lexer.h"
+#define ERROR_COLOR "\001\033[1;31m\002"
+#define RESET_COLOR "\001\033[0m\002"
+
+// Add before process_line function
+int check_syntax(const char *line);
+extern "C" void redisplay_hook();
+int is_quoted(char *text, int index);
+
+#endif
