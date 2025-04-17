3fefad22b009dda18084337819dbbcec17bbfe71
Prashasta Srivastava <147490436+PrashastS1@users.noreply.github.com>
2025-04-17 18:23:47 +0530

    basic changes

--- shell/include/lexer.h
+++ shell/include/lexer.h
@@ -1,3 +1,30 @@
+// // // #ifndef LEXER_H
+// // // #define LEXER_H
+
+// // // #include <vector>
+// // // #include <string>
+
+// // // enum class TokenType {
+// // //     IDENTIFIER,
+// // //     NUMBER,
+// // //     PIPE,
+// // //     REDIRECT_OUT,
+// // //     REDIRECT_IN,
+// // //     END
+// // // };
+
+// // // struct Token {
+// // //     TokenType type;
+// // //     std::string value;
+// // // };
+
+// // // class Lexer {
+// // // public:
+// // //     std::vector<Token> tokenize(const std::string& input);
+// // // };
+
+// // // #endif
+
 // // #ifndef LEXER_H
 // // #define LEXER_H
 
@@ -6,16 +33,25 @@
 
 // // enum class TokenType {
 // //     IDENTIFIER,
-// //     NUMBER,
+// //     NUMBER, // Still unused? Consider removing if not planned.
 // //     PIPE,
 // //     REDIRECT_OUT,
 // //     REDIRECT_IN,
 // //     END
+// //     // Add other operators like >>, & if needed
+// // };
+
+// // // Define how a token was quoted (or not)
+// // enum class QuotingType {
+// //     NONE,         // No quotes
+// //     SINGLE,       // Enclosed in '...'
+// //     DOUBLE        // Enclosed in "..."
 // // };
 
 // // struct Token {
 // //     TokenType type;
 // //     std::string value;
+// //     QuotingType quoting = QuotingType::NONE; // Default to no quoting
 // // };
 
 // // class Lexer {
@@ -23,43 +59,7 @@
 // //     std::vector<Token> tokenize(const std::string& input);
 // // };
 
-// // #endif
-
-// #ifndef LEXER_H
-// #define LEXER_H
-
-// #include <vector>
-// #include <string>
-
-// enum class TokenType {
-//     IDENTIFIER,
-//     NUMBER, // Still unused? Consider removing if not planned.
-//     PIPE,
-//     REDIRECT_OUT,
-//     REDIRECT_IN,
-//     END
-//     // Add other operators like >>, & if needed
-// };
-
-// // Define how a token was quoted (or not)
-// enum class QuotingType {
-//     NONE,         // No quotes
-//     SINGLE,       // Enclosed in '...'
-//     DOUBLE        // Enclosed in "..."
-// };
-
-// struct Token {
-//     TokenType type;
-//     std::string value;
-//     QuotingType quoting = QuotingType::NONE; // Default to no quoting
-// };
-
-// class Lexer {
-// public:
-//     std::vector<Token> tokenize(const std::string& input);
-// };
-
-// #endif // LEXER_H
+// // #endif // LEXER_H
 
 #ifndef LEXER_H
 #define LEXER_H
--- shell/src/lexer.cpp
+++ shell/src/lexer.cpp
@@ -1,120 +1,120 @@
-// #include "lexer.h"
-// #include <iostream> // For potential error messages
-// #include <cctype>   // For isspace, isalnum, etc.
+// // #include "lexer.h"
+// // #include <iostream> // For potential error messages
+// // #include <cctype>   // For isspace, isalnum, etc.
 
-// enum class LexerState {
-//     DEFAULT,
-//     IN_SQUOTE,
-//     IN_DQUOTE
-// };
+// // enum class LexerState {
+// //     DEFAULT,
+// //     IN_SQUOTE,
+// //     IN_DQUOTE
+// // };
 
-// std::vector<Token> Lexer::tokenize(const std::string& input) {
+// // std::vector<Token> Lexer::tokenize(const std::string& input) {
 
-//     std::vector<Token> tokens;
-//     std::string currentTokenValue;
-//     LexerState currentState = LexerState::DEFAULT;
-//     QuotingType currentQuoting = QuotingType::NONE;
+// //     std::vector<Token> tokens;
+// //     std::string currentTokenValue;
+// //     LexerState currentState = LexerState::DEFAULT;
+// //     QuotingType currentQuoting = QuotingType::NONE;
 
-//     auto emitToken = [&](TokenType type) {
-//         bool canBeEmpty = (type == TokenType::END || type == TokenType::PIPE ||
-//                            type == TokenType::REDIRECT_IN || type == TokenType::REDIRECT_OUT);
+// //     auto emitToken = [&](TokenType type) {
+// //         bool canBeEmpty = (type == TokenType::END || type == TokenType::PIPE ||
+// //                            type == TokenType::REDIRECT_IN || type == TokenType::REDIRECT_OUT);
 
-//         if (!currentTokenValue.empty() || canBeEmpty) {
-//              if (type == TokenType::IDENTIFIER && !currentTokenValue.empty()) {
-//                  if (currentTokenValue == "|") type = TokenType::PIPE;
-//                  else if (currentTokenValue == ">") type = TokenType::REDIRECT_OUT;
-//                  else if (currentTokenValue == "<") type = TokenType::REDIRECT_IN;
-//              }
+// //         if (!currentTokenValue.empty() || canBeEmpty) {
+// //              if (type == TokenType::IDENTIFIER && !currentTokenValue.empty()) {
+// //                  if (currentTokenValue == "|") type = TokenType::PIPE;
+// //                  else if (currentTokenValue == ">") type = TokenType::REDIRECT_OUT;
+// //                  else if (currentTokenValue == "<") type = TokenType::REDIRECT_IN;
+// //              }
 
-//             if (type != TokenType::IDENTIFIER) {
-//                 currentQuoting = QuotingType::NONE;
-//             }
+// //             if (type != TokenType::IDENTIFIER) {
+// //                 currentQuoting = QuotingType::NONE;
+// //             }
 
-//             Token newToken;
-//             newToken.type = type;
-//             newToken.value = currentTokenValue;
-//             newToken.quoting = currentQuoting;
-//             tokens.push_back(newToken);
+// //             Token newToken;
+// //             newToken.type = type;
+// //             newToken.value = currentTokenValue;
+// //             newToken.quoting = currentQuoting;
+// //             tokens.push_back(newToken);
 
-//             currentTokenValue.clear();
-//             currentQuoting = QuotingType::NONE;
-//         }
-//     };
+// //             currentTokenValue.clear();
+// //             currentQuoting = QuotingType::NONE;
+// //         }
+// //     };
 
-//     for (char c : input) {
-//         switch (currentState) {
-//             case LexerState::DEFAULT:
-//                 if (c == '\'') {
-//                     emitToken(TokenType::IDENTIFIER); // Emit anything prior
-//                     currentState = LexerState::IN_SQUOTE;
-//                     currentQuoting = QuotingType::SINGLE; // Set quoting for the token *about* to be built
-//                 } else if (c == '"') {
-//                     emitToken(TokenType::IDENTIFIER);
-//                     currentState = LexerState::IN_DQUOTE;
-//                     currentQuoting = QuotingType::DOUBLE;
-//                 } else if (c == '|') {
-//                     emitToken(TokenType::IDENTIFIER);
-//                     currentTokenValue += c;
-//                     emitToken(TokenType::PIPE);
-//                 } else if (c == '>') {
-//                     emitToken(TokenType::IDENTIFIER);
-//                     currentTokenValue += c;
-//                     emitToken(TokenType::REDIRECT_OUT);
-//                 } else if (c == '<') {
-//                     emitToken(TokenType::IDENTIFIER);
-//                     currentTokenValue += c;
-//                     emitToken(TokenType::REDIRECT_IN);
-//                 } else if (isspace(c)) {
-//                     emitToken(TokenType::IDENTIFIER); // Emit previous token
-//                 } else {
-//                     // Part of an unquoted identifier
-//                     currentQuoting = QuotingType::NONE; // Ensure it's marked as NONE
-//                     currentTokenValue += c;
-//                 }
-//                 break;
+// //     for (char c : input) {
+// //         switch (currentState) {
+// //             case LexerState::DEFAULT:
+// //                 if (c == '\'') {
+// //                     emitToken(TokenType::IDENTIFIER); // Emit anything prior
+// //                     currentState = LexerState::IN_SQUOTE;
+// //                     currentQuoting = QuotingType::SINGLE; // Set quoting for the token *about* to be built
+// //                 } else if (c == '"') {
+// //                     emitToken(TokenType::IDENTIFIER);
+// //                     currentState = LexerState::IN_DQUOTE;
+// //                     currentQuoting = QuotingType::DOUBLE;
+// //                 } else if (c == '|') {
+// //                     emitToken(TokenType::IDENTIFIER);
+// //                     currentTokenValue += c;
+// //                     emitToken(TokenType::PIPE);
+// //                 } else if (c == '>') {
+// //                     emitToken(TokenType::IDENTIFIER);
+// //                     currentTokenValue += c;
+// //                     emitToken(TokenType::REDIRECT_OUT);
+// //                 } else if (c == '<') {
+// //                     emitToken(TokenType::IDENTIFIER);
+// //                     currentTokenValue += c;
+// //                     emitToken(TokenType::REDIRECT_IN);
+// //                 } else if (isspace(c)) {
+// //                     emitToken(TokenType::IDENTIFIER); // Emit previous token
+// //                 } else {
+// //                     // Part of an unquoted identifier
+// //                     currentQuoting = QuotingType::NONE; // Ensure it's marked as NONE
+// //                     currentTokenValue += c;
+// //                 }
+// //                 break;
 
-//             case LexerState::IN_SQUOTE:
-//                 if (c == '\'') {
-//                     // End of single-quoted string. currentQuoting is already SINGLE.
-//                     emitToken(TokenType::IDENTIFIER);
-//                     currentState = LexerState::DEFAULT;
-//                 } else {
-//                     currentTokenValue += c;
-//                 }
-//                 break;
+// //             case LexerState::IN_SQUOTE:
+// //                 if (c == '\'') {
+// //                     // End of single-quoted string. currentQuoting is already SINGLE.
+// //                     emitToken(TokenType::IDENTIFIER);
+// //                     currentState = LexerState::DEFAULT;
+// //                 } else {
+// //                     currentTokenValue += c;
+// //                 }
+// //                 break;
 
-//             case LexerState::IN_DQUOTE:
-//                 // Basic: No escape sequences handled yet
-//                 if (c == '"') {
-//                     // End of double-quoted string. currentQuoting is already DOUBLE.
-//                     emitToken(TokenType::IDENTIFIER);
-//                     currentState = LexerState::DEFAULT;
-//                 } else {
-//                     currentTokenValue += c;
-//                 }
-//                 break;
-//         }
-//     } // End of character loop
+// //             case LexerState::IN_DQUOTE:
+// //                 // Basic: No escape sequences handled yet
+// //                 if (c == '"') {
+// //                     // End of double-quoted string. currentQuoting is already DOUBLE.
+// //                     emitToken(TokenType::IDENTIFIER);
+// //                     currentState = LexerState::DEFAULT;
+// //                 } else {
+// //                     currentTokenValue += c;
+// //                 }
+// //                 break;
+// //         }
+// //     } // End of character loop
 
 
-//     if (currentState != LexerState::DEFAULT) {
-//         std::cerr << "alpShell: Lexer Error: Unterminated quote." << std::endl;
-//         tokens.clear();
-//         return tokens;
-//     }
+// //     if (currentState != LexerState::DEFAULT) {
+// //         std::cerr << "alpShell: Lexer Error: Unterminated quote." << std::endl;
+// //         tokens.clear();
+// //         return tokens;
+// //     }
 
-//     // Emit any remaining token accumulated in DEFAULT state
-//     // emitToken(TokenType::IDENTIFIER);
-//     emitToken(TokenType::IDENTIFIER);
+// //     // Emit any remaining token accumulated in DEFAULT state
+// //     // emitToken(TokenType::IDENTIFIER);
+// //     emitToken(TokenType::IDENTIFIER);
 
-//     Token endToken;
-//     endToken.type = TokenType::END;
-//     endToken.value = "";
-//     endToken.quoting = QuotingType::NONE;
-//     tokens.push_back(endToken);
+// //     Token endToken;
+// //     endToken.type = TokenType::END;
+// //     endToken.value = "";
+// //     endToken.quoting = QuotingType::NONE;
+// //     tokens.push_back(endToken);
 
-//     return tokens;
-// }
+// //     return tokens;
+// // }
 
 #include "lexer.h"
 #include <iostream>
